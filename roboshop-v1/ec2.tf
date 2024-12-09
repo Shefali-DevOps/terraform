c7c824f00df6160bce3ec57cc8b3238eb70a3555
Shefali <shefalimurarka19@gmail.com>
2024-12-09 21:30:49 +0530

    Terraform code

--- roboshop-v1/ec2.tf
+++ roboshop-v1/ec2.tf
@@ -1,17 +1,17 @@
 resource "aws_instance" "frontend"{
-  ami = ""
+  ami = "ami-09c813fb71547fc4f"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [""]
+  vpc_security_group_ids = ["sg-05fc8f00bef558456"]
 
   tags = {
     name="frontend.dev"
   }
 }
 
-resource "aws_route53_record" "" {
+resource "aws_route53_record" "frontend" {
   name    = "frontend.dev.shefalidevops.shop"
   type    = "A"
-  zone_id = ""
+  zone_id = "Z04468998YJS3W1N2Q1P"
   ttl =  15
   records = [aws_instance.frontend.private_ip]
 }
