1b4294400cb5a6cf211240820c3873fd5e6b44d0
Shefali <shefalimurarka19@gmail.com>
2024-12-12 11:57:00 +0530

    Terraform code

--- modules-2/dns/main.tf
+++ modules-2/dns/main.tf
@@ -1,7 +1,7 @@
-resource "aws_route53_record" "dns" {
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z04468998YJS3W1N2Q1P"
   name    = "test.dev.shefalidevops.shop"
   type    = "A"
-  zone_id = "Z04468998YJS3W1N2Q1P"
   ttl =  15
   records = [var.private_ip]
 }
