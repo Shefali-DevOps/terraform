79916378c18f0ac4d1da016928e828b48dfe8ec7
Shefali <shefalimurarka19@gmail.com>
2024-12-15 13:29:10 +0530

    Terraform code

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -2,7 +2,7 @@ resource "null_resource" "test"{}
 
   terraform{
     backend "s3"{
-      bucket = "terraform-state"
+      bucket = "shefali-terraform-state"
       key = "test/terraform.tfstate"
       region = "us-east-1"
     }
