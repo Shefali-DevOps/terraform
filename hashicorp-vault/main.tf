985321a0d4a07fa7072be1fc99fb88a1707e322c
Shefali <shefalimurarka19@gmail.com>
2024-12-21 16:34:54 +0530

    terraform hashicorp vault

---
+++ hashicorp-vault/main.tf
@@ -0,0 +1,16 @@
+variable "vault_token" {}
+
+data "vault_generic_secret" "rundeck_auth" {
+  path = "kv/test"
+}
+
+provider "vault" {
+  address         = "https://vault-internal.shefalidevops.shop:8200"
+  token           = var.vault_token
+  skip_tls_verify = true
+}
+
+resource "local_file" "foo" {
+  content  = data.vault_generic_secret.rundeck_auth.data["MYPASS"]
+  filename = "/tmp/foo.bar"
+}
